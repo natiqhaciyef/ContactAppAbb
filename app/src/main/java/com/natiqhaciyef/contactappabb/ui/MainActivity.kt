c44fc1b51ddd5fc5664758f6d94770fe8298d3c6
Natiq <natiq00h2272@gmail.com>
2022-11-12 11:38:11 +0400

    Initial commit

---
+++ app/src/main/java/com/natiqhaciyef/contactappabb/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+﻿package com.natiqhaciyef.contactappabb.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.natiqhaciyef.contactappabb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
